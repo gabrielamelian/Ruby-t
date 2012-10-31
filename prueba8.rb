16ec788a92bf9e3f8ae4c93597b4fabac5ca488b
Gabriela ROQUE LOPEZ <gabrielamelian@gmail.com>
2012-10-31 13:41:19 +1300

    commit work with pedro

--- prueba8.rb
+++ prueba8.rb
@@ -1,6 +1,6 @@
 puts 'The Amazing Alphabetizer'.center(60)
 puts 'Type all the words you want and we will put them in alphabetic order for you'
-puts 'It\s easy! Just write the words followed by the \'Enter\' key. Once you finish just enter a blank line'
+puts 'It\'s easy! Just write the words followed by the \'Enter\' key. Once you finish just enter a blank line'
 
 list = []
 word = gets.chomp
